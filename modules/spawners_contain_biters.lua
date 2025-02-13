6dfe7c5572b10628cd8052043742cccc35ca2d0e
TheBigZet <92057893+TheBigZet@users.noreply.github.com>
2025-02-13 12:31:13 +0100

    omit numeric keys in biter_building_inhabitants localize surface and position before loops

--- modules/spawners_contain_biters.lua
+++ modules/spawners_contain_biters.lua
@@ -4,16 +4,16 @@ local event = require('utils.event')
 local math_random = math.random
 
 local biter_building_inhabitants = {
-    [1] = { { 'small-biter', 8, 16 } },
-    [2] = { { 'small-biter', 12, 24 } },
-    [3] = { { 'small-biter', 8, 16 }, { 'medium-biter', 1, 2 } },
-    [4] = { { 'small-biter', 4, 8 }, { 'medium-biter', 4, 8 } },
-    [5] = { { 'small-biter', 3, 5 }, { 'medium-biter', 8, 12 } },
-    [6] = { { 'small-biter', 3, 5 }, { 'medium-biter', 5, 7 }, { 'big-biter', 1, 2 } },
-    [7] = { { 'medium-biter', 6, 8 }, { 'big-biter', 3, 5 } },
-    [8] = { { 'medium-biter', 2, 4 }, { 'big-biter', 6, 8 } },
-    [9] = { { 'medium-biter', 2, 3 }, { 'big-biter', 7, 9 } },
-    [10] = { { 'big-biter', 4, 8 }, { 'behemoth-biter', 3, 4 } },
+    { { 'small-biter', 8, 16 } },
+    { { 'small-biter', 12, 24 } },
+    { { 'small-biter', 8, 16 }, { 'medium-biter', 1, 2 } },
+    { { 'small-biter', 4, 8 }, { 'medium-biter', 4, 8 } },
+    { { 'small-biter', 3, 5 }, { 'medium-biter', 8, 12 } },
+    { { 'small-biter', 3, 5 }, { 'medium-biter', 5, 7 }, { 'big-biter', 1, 2 } },
+    { { 'medium-biter', 6, 8 }, { 'big-biter', 3, 5 } },
+    { { 'medium-biter', 2, 4 }, { 'big-biter', 6, 8 } },
+    { { 'medium-biter', 2, 3 }, { 'big-biter', 7, 9 } },
+    { { 'big-biter', 4, 8 }, { 'behemoth-biter', 3, 4 } },
 }
 
 local function on_entity_died(event)
@@ -27,11 +27,13 @@ local function on_entity_died(event)
     if e < 1 then
         e = 1
     end
+    local entity_surface = event.entity.surface
+    local entity_position = event.entity.position
     for _, t in pairs(biter_building_inhabitants[e]) do
         for x = 1, math_random(t[2], t[3]), 1 do
-            local p = event.entity.surface.find_non_colliding_position(t[1], event.entity.position, 6, 1)
+            local p = entity_surface.find_non_colliding_position(t[1], entity_position, 6, 1)
             if p then
-                event.entity.surface.create_entity({ name = t[1], position = p, force = event.entity.force.name })
+                entity_surface.create_entity({ name = t[1], position = p, force = event.entity.force.name })
             end
         end
     end
